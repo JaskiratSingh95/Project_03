e62ba145ff9250944283bb6d9262a12d4c05fcb8
Y1U9M9A4I <situ.ym@gmail.com>
2024-06-18 21:39:00 -0400

    all skeleton source codes for group edit + notebook

--- project3.js
+++ project3.js
@@ -1,10 +1,7 @@
 // Get the webpage endpoint
-const proj3 = "OURLINK";
+const proj3 = "OURLINK/json";
 
 // Fetch the JSON data and console log it
 d3.json(proj3).then(function(data) {
   console.log(data);
 });
-
-// flask route is in the json instead of the url here 
-//(eg. forward/data) --> will run this route 
