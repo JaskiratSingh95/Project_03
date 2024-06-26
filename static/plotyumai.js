c180c5b8d4727b4f16f149862b83a6cd841db83c
Y1U9M9A4I <situ.ym@gmail.com>
2024-06-25 22:23:47 -0400

    yumai all final files into conflict free branch

--- static/plotyumai.js
+++ static/plotyumai.js
@@ -3,43 +3,61 @@ fetch('http://127.0.0.1:5000/api/v1.0/stroke_biological_predictors_data')
     .then(data => {
         console.log('Fetched data:', data);
 
-        Highcharts.chart('container', {
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Swarm Plot for Glucose Level vs. BMI vs. Stroke'
-            },
-            xAxis: {
+        function createScatterPlot(data) {
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'scatter',
+                    zoomType: 'xy'
+                },
                 title: {
-                    text: 'Average Glucose Level'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'BMI'
-                }
-            },
-            series: [{
-                name: 'Kaggle: stroke-prediction-dataset',
-                data: data.map(d => ({
-                    x: parseFloat(d.bmi),
-                    y: parseFloat(d.avg_glucose_level),
-                    color: d.stroke === 1 ? 'red' : 'green'
-                }))
-            }],
-            plotOptions: {
-                series: {
-                    turboThreshold: data.length,
-                    marker: {
-                        radius: 1 // smaller radius for datapoints
+                    text: 'Swarm Plot for Glucose Level vs. BMI vs. Stroke'
+                },
+                xAxis: {
+                    title: {
+                        text: 'BMI'
                     }
+                },
+                yAxis: {
+                    title: {
+                        text: 'Average Glucose Level'
+                    }
+                },
+                series: [{
+                    name: 'Non-Stroke Participants',
+                    data: data.filter(d => d.stroke === 1).map(d => ({
+                        x: parseFloat(d.bmi),
+                        y: parseFloat(d.avg_glucose_level),
+                        color: 'lime'
+                    }))
+                }, {
+                    name: 'Stroke Participants',
+                    data: data.filter(d => d.stroke === 0).map(d => ({
+                        x: parseFloat(d.bmi),
+                        y: parseFloat(d.avg_glucose_level),
+                        color: '#e0b0ff'
+                    }))
+                }],
+                plotOptions: {
+                    series: {
+                        turboThreshold: data.length,
+                        marker: {
+                            radius: 2,
+                            fillOpacity: 0.5
+                        }
+                    }
+                },
+                accessibility: {
+                    enabled: true
+                },
+                legend: {
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    layout: 'vertical',
+                    symbolRadius: 15
                 }
-            },
-            accessibility: {
-                enabled: true // Enable accessibility module
-            }
-        });
+            });
+        }
+
+        createScatterPlot(data);
     })
     .catch(error => console.error('Error fetching or processing scatterplot data:', error));
